033a55a2306c2a7ff9869343b0f6ce2a4b2eba52
LuisPulenta <luisalbertonu@gmail.com>
2022-10-17 12:26:00 -0300

    Video 325

--- android/app/src/main/kotlin/com/luisnu/authapp/MainActivity.kt
+++ android/app/src/main/kotlin/com/luisnu/authapp/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.example.authapp
+package com.luisnu.authapp
 
 import io.flutter.embedding.android.FlutterActivity
 
---
+++ lib/services/google_signin_service.dart
@@ -0,0 +1,32 @@
+import 'package:google_sign_in/google_sign_in.dart';
+
+class GoogleSignInService {
+  static GoogleSignIn _googleSignIn = GoogleSignIn(
+    scopes: [
+      'email',
+    ],
+  );
+
+  static Future<GoogleSignInAccount?> signInWithGoogle() async {
+    try {
+      final account = await _googleSignIn.signIn();
+
+      final googleKey = await account!.authentication;
+      print('=========== ID TOKEN ============');
+      print(account);
+      print(googleKey.idToken);
+
+      //TODO: Llamar un servicio REST a nuestro backend con el ID Token
+
+      return account;
+    } catch (e) {
+      print('Error en Google SignIn');
+      print(e);
+      return null;
+    }
+  }
+
+  static Future signOut() async {
+    await _googleSignIn.signOut();
+  }
+}
