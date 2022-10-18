3b875cad67d1bd131c04c3459621240a027191c9
LuisPulenta <luisalbertonu@gmail.com>
2022-10-18 11:27:06 -0300

    Video 333

--- lib/services/google_signin_service.dart
+++ lib/services/google_signin_service.dart
@@ -1,4 +1,5 @@
 import 'package:google_sign_in/google_sign_in.dart';
+import 'package:http/http.dart' as http;
 
 class GoogleSignInService {
   static GoogleSignIn _googleSignIn = GoogleSignIn(
@@ -12,11 +13,22 @@ class GoogleSignInService {
       final account = await _googleSignIn.signIn();
 
       final googleKey = await account!.authentication;
-      print('=========== ID TOKEN ============');
-      print(account);
-      print(googleKey.idToken);
+      // print('=========== ID TOKEN ============');
+      // print(account);
+      // print(googleKey.idToken);
 
       //TODO: Llamar un servicio REST a nuestro backend con el ID Token
+
+      final signInWithGoogleEndpoint = Uri(
+          scheme: 'https',
+          host: 'google-sign-in-luisnu.herokuapp.com',
+          path: '/google');
+
+      final session = await http
+          .post(signInWithGoogleEndpoint, body: {'token': googleKey.idToken});
+
+      print('========== BACKEND ==========');
+      print(session.body);
 
       return account;
     } catch (e) {
